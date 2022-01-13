1bd16b4ce4258a47239a463f839725fa51dd12d5
MarwaAlrawahi98 <97635133+MarwaAlrawahi98@users.noreply.github.com>
2022-01-13 18:33:22 +0400

    Add files via upload

---
+++ telypayController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication6.Controllers
+{
+    public class telypayController : ApiController
+    {
+        // GET: api/telypay
+        [HttpGet]  
+        [Route("api/SignIN/{userName}/{password}")]
+        public IHttpActionResult SignIN(string userName, string password)
+        {
+            if (userName.Equals("telypay"))
+            {
+                if (password.Equals("password123"))
+                {
+
+                    return Ok("Success");
+                }
+                else
+                {
+                    return BadRequest("Wrong Password");
+                }
+            }
+
+            return InternalServerError(new Exception("error"));
+
+        }
+
+    }
+}
